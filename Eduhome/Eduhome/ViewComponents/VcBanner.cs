9ba145ca89a225d1f6a28e544859c8e0bd7ca751
HamletHajiyev <hamletoh@code.edu.az>
2021-12-10 23:18:26 +0400

    Asp.Net Core Admin Panel

---
+++ Eduhome/Eduhome/ViewComponents/VcBanner.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eduhome.ViewComponents
+{
+    public class VcBanner : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
